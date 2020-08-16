19f5efefc216889ce2eb6a2900a01f39e2b3dd13
sengulali <aslandamla46@gmail.com>
2020-08-16 19:29:50 +0300

    ilk commit from master

--- src/git_project_brach/BranchDeneme.java
+++ src/git_project_brach/BranchDeneme.java
@@ -3,8 +3,8 @@ package git_project_brach;
 public class BranchDeneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("16:28 - master branchten ilk commit");
 	}
 
 }
